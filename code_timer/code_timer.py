db7da5b12d83357cc71ff58aff17d03cbbec2f7a
gulaki <gulaki@users.noreply.github.com>
2018-03-07 18:13:15 +0530

    reformatted readme and docs

--- code_timer/code_timer.py
+++ code_timer/code_timer.py
@@ -10,34 +10,34 @@ class Timer:
         using time.clock()
         Useful for benchmarking and code improvement.
 
-        >>> t = Timer()  # starts the timer object
-        ...
-        >>> t.stop()  # stops the timer object
-        >>> t.total  # then contains total time between creation and stop() in seconds.
-        >>> repr(t)  # returns time with units as string, so print(t) can be used.
+            >>> t = Timer()  # starts the timer object
+            ...
+            >>> t.stop()  # stops the timer object
+            >>> t.total  # then contains total time between creation and stop() in seconds.
+            >>> repr(t)  # returns time with units as string, so print(t) can be used.
 
-        >>> t = Timer(wait=True)  # creates timer but doesn't start measuring. t can be passed to functions to measure time
+            >>> t = Timer(wait=True)  # creates timer but doesn't start measuring. t can be passed to functions to measure time
         Wait is False by default. See previous case.
 
-        >>> t.start()  # begins measuring.
-        ...
-        >>> t.stop()  # stops measuring.
-        ...
-        >>> t.start() # starts measuring again
-        ...
-        >>> t.stop()  # stops measuring
-        >>> t.total  # now has total time recorded between starts and stops in seconds
-        >>> t.count  # contains number of times the timer was triggered
+            >>> t.start()  # begins measuring.
+            ...
+            >>> t.stop()  # stops measuring.
+            ...
+            >>> t.start() # starts measuring again
+            ...
+            >>> t.stop()  # stops measuring
+            >>> t.total  # now has total time recorded between starts and stops in seconds
+            >>> t.count  # contains number of times the timer was triggered
 
         This can be used in a loop to measure time per loop or count number of times a function was called.
 
-        >>> t = Timer(unit='s')  # to define what units to print the time in.
+            >>> t = Timer(unit='s')  # to define what units to print the time in.
 
         Options are 's' (second), 'ms' (millisecond), 'm' (minute) and 'h' (hour).
 
         By default the unit is 'ms' but in a project the default units can be set before all usages by
 
-        >>> Timer.default_unit = 's'  # or any unit
+            >>> Timer.default_unit = 's'  # or any unit
     """
     default_unit = 'ms'
 
