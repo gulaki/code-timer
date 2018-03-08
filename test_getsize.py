ced3ae6c940b22559f90da2b69e6727589022491
gulaki <gulaki@users.noreply.github.com>
2018-03-09 00:05:09 +0530

    get_size
    WIP

---
+++ test_getsize.py
@@ -0,0 +1,20 @@
+from codetimer import *
+import random
+import matplotlib.pyplot as plt
+
+data1 = []
+data2 = []
+
+mem = MemoryTracker(data1, data2)
+
+for i in range(10000):
+    for _ in range(random.randint(0, 2)):
+        data1.append([random.random() for _ in range(100)])
+    for _ in range(random.randint(1, 2)):
+        data2.append([random.random() for _ in range(700)])
+    mem.getsize(data1, data2)
+
+plt.subplot()
+plt.plot(mem.times, mem.sizes)
+plt.show()
+print('Test')
