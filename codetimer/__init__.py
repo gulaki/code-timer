d2f8d5de58d1878062b2ace537458bfadae280d3
gulaki <35985508+gulaki@users.noreply.github.com>
2018-03-08 16:10:22 +0530

    Update __init__.py

--- codetimer/__init__.py
+++ codetimer/__init__.py
@@ -1,1 +1,6 @@
 from .codetimer import Timer, FunctionTimer, TimerStats, FunctionTimerStats
+
+__author__ = 'Anustuv Pal'
+__email__ = 'anustuv@gmail.com'
+__url__ = 'https://github.com/gulaki'
+__license__ = 'MIT'
